41e4d5e6d605734eb6d62ba0aead0ae9792d23e6
John de la Garza <jdelagarza@cylance.com>
2019-09-25 08:21:10 -0700

    ocalc: refactor

--- ocalc.py
+++ ocalc.py
@@ -9,7 +9,14 @@ class Food:
         self.protein_g = weight_g * self.protein_g_per_g
 
     def __repr__(self):
-        return f'g{self.weight_g}, c{self.energy_c}, f{self.fat_g}, p{self.protein_g}'
+        return ', '.join((
+            self.__class__.__name__,
+            f'grams {int(self.weight_g)}',
+            f'calories {int(self.energy_c)}',
+            f'fat {int(self.fat_g)}',
+            f'protein {int(self.protein_g)}',
+        ))
+
 
 class Meat(Food):
     energy_c_per_g = None
@@ -34,17 +41,21 @@ class Suet(Food):
     protein_g_per_g = .01
 
 
-def meal(total_energy_c, total_protein_g, liver_g_per_w):
-    liver = Liver(liver_g_per_w/7)
-    meat_protein_g = total_protein_g - liver.protein_g
+def meal(energy_c, protein_g, liver_g):
+    liver = Liver(liver_g)
+    meat_protein_g = protein_g - liver.protein_g
     prop_fat=.5
-    meat = Meat(meat_protein_g/Meat(1, prop_fat).protein_g_per_g, prop_fat)
+    meat_protein_g_per_g = Meat(1, prop_fat).protein_g_per_g
+    meat = Meat(meat_protein_g / meat_protein_g_per_g, prop_fat)
     suet = Suet(
-        (total_energy_c - (liver.energy_c + meat.energy_c))
+        (energy_c - (liver.energy_c + meat.energy_c))
         / Suet.energy_c_per_g)
     return [f'{i}: {int(f.weight_g)}' for i, f
             in (('liver', liver), ('meat', meat), ('suet', suet))]
 
 if __name__ == '__main__':
-    for i in meal(total_energy_c=2000, total_protein_g=64, liver_g_per_w=454):
+    liver_g_per_w = 454
+    liver_g_per_d = liver_g_per_w / 7
+    m =  meal(energy_c=2000, protein_g=64, liver_g=liver_g_per_d)
+    for i in m:
         print(i)
