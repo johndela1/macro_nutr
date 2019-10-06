cee661e79cedcdaf7d6e94ed1ed03cad1dc82c80
John de la Garza <jdelagarza@cylance.com>
2019-10-06 14:31:39 -0700

    support multiple meats

--- ocalc.py
+++ ocalc.py
@@ -11,13 +11,10 @@ import sys
 
 class Food:
     carb_g_per_g = 0
-    def __init__(self):
-        self.energy_kc_per_g = self.carb_g_per_g*4 + self.fat_g_per_g*9 + self.protein_g_per_g*4
-        self._weight_g = 0
-        self.energy_kc = 0
-        self.carb_g = 0
-        self.fat_g = 0
-        self.protein_g = 0
+    def __init__(self, weight_g=0):
+        self.energy_kc_per_g = (
+            self.carb_g_per_g*4 + self.fat_g_per_g*9 + self.protein_g_per_g*4)
+        self.weight_g = weight_g
 
     @property
     def weight_g(self):
@@ -43,7 +40,8 @@ class Food:
 
 
 class Fat(Food):
-    pass
+    fat_g_per_g = .946
+    protein_g_per_g = .018
 
 
 class Meat(Food):
@@ -54,7 +52,7 @@ class Organ(Food):
     pass
 
 
-class GB45(Fat):
+class GB45(Meat):
     fat_g_per_g = .441
     protein_g_per_g = .127
 
@@ -65,44 +63,43 @@ class Liver(Organ):
     protein_g_per_g = .204
 
 
-class Ribeye(Food):
+class Ribeye(Meat):
     fat_g_per_g = .221
     protein_g_per_g = .175
 
 
-class Suet(Fat):
-    fat_g_per_g = .946
-    protein_g_per_g = .018
-
-
-def meal(foods, energy_kc, protein_g, organ_g):
-    fat, meat, organ = foods
-    organ.weight_g = organ_g
-    meat.weight_g = (protein_g - organ.protein_g) / meat.protein_g_per_g
-    fat.weight_g = (energy_kc - (organ.energy_kc + meat.energy_kc)) / fat.energy_kc_per_g
-    return foods
-
-
-def fat_prop_meal(energy_kc, fat_ratio, meat_fat_prop, liver_g):
-    protein_g = energy_kc/(fat_ratio*9 + 4)
-    return meal(energy_kc, protein_g, meat_fat_prop, liver_g)
-
-
 def fat_prop(meal):
-    fat_g = sum(i.fat_g for i in meal)
-    protein_g= sum(i.protein_g for i in meal)
-    return fat_g / protein_g
+    return (sum(food.fat_g for food in meal)
+            / sum(food.protein_g for food in meal))
+
+
+def by_type(foods, type_):
+    return [f for f in foods if isinstance(f, type_)]
 
 
 if __name__ == '__main__':
-    fat = Suet()
-    meat = GB45()
-    organ = Liver()
-    foods = fat, meat, organ
+    energy_kc = 1900
+    protein_g=64
+    organ_g = 454 / 7
+    foods = (
+        Fat(),
+        GB45(),
+        Liver(organ_g),
+        Ribeye(),
+    )
 
-    organ_g_per_w = 454
-    organ_g_per_d = organ_g_per_w / 7
-    m =  meal(foods, energy_kc=1900, protein_g=64, organ_g=organ_g_per_d)
-    for i in m:
-        print(i)
-    print('fat/protein ', fat_prop(m))
+    fat = by_type(foods, Fat)[0]
+    meats = by_type(foods, Meat)
+    organ = by_type(foods, Organ)[0]
+
+    protein_g_per_meat = (protein_g - organ.protein_g) / len(meats)
+    for meat in meats:
+        meat.weight_g = protein_g_per_meat / meat.protein_g_per_g
+
+    fat.weight_g = (energy_kc
+                    - (organ.energy_kc + sum(meat.energy_kc for meat in meats)
+                    )) / fat.energy_kc_per_g
+
+    for food in foods:
+        print(food)
+    print('fat/protein ', fat_prop(foods))
