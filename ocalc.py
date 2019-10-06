85540908528d4e67ba4ca4f0970bd229b28ec117
John de la Garza <jdelagarza@cylance.com>
2019-10-06 12:35:00 -0700

    create new Food class

--- ocalc.py
+++ ocalc.py
@@ -9,13 +9,28 @@ https://www.nutritionvalue.org/
 
 import sys
 
-
 class Food:
-    def __init__(self, weight_g):
-        self.weight_g = weight_g
-        self.energy_kc = weight_g * self.energy_kc_per_g
-        self.fat_g = weight_g * self.fat_g_per_g
-        self.protein_g = weight_g * self.protein_g_per_g
+    carb_g_per_g = 0
+    def __init__(self):
+        self.energy_kc_per_g = self.carb_g_per_g*4 + self.fat_g_per_g*9 + self.protein_g_per_g*4
+        self._weight_g = 0
+        self.energy_kc = 0
+        self.carb_g = 0
+        self.fat_g = 0
+        self.protein_g = 0
+
+    @property
+    def weight_g(self):
+        return self._weight_g
+
+    @weight_g.setter
+    def weight_g(self, value):
+        self._weight_g = value
+        self.energy_kc = self.energy_kc_per_g * value
+        self.carb_g = self.carb_g_per_g * value
+        self.fat_g = self.fat_g_per_g * value
+        self.protein_g = self.protein_g_per_g * value
+
 
     def __repr__(self):
         return ', '.join((
@@ -27,42 +42,45 @@ class Food:
         ))
 
 
+class Fat(Food):
+    pass
+
+
 class Meat(Food):
-    energy_kc_per_g = None
-    fat_g_per_g = None
-    protein_g_per_g = None
-
-    def __init__(self, weight_g, fat_prop):
-        self.fat_g_per_g = fat_prop * .98
-        self.protein_g_per_g = (1-fat_prop) * .23
-        self.energy_kc_per_g = self.fat_g_per_g*9 + self.protein_g_per_g*4
-        super().__init__(weight_g)
-
-    @classmethod
-    def from_protein_g(cls, protein_g, fat_prop):
-        protein_g_per_g = cls(1, fat_prop).protein_g_per_g
-        return cls(protein_g / protein_g_per_g, fat_prop)
+    pass
 
 
-class Liver(Food):
-    energy_kc_per_g = 1.35
+class Organ(Food):
+    pass
+
+
+class GB45(Fat):
+    fat_g_per_g = .441
+    protein_g_per_g = .127
+
+
+class Liver(Organ):
+    carb_g_per_g = .039
     fat_g_per_g = .036
     protein_g_per_g = .204
 
 
-class Suet(Food):
-    energy_kc_per_g = 8.54
+class Ribeye(Food):
+    fat_g_per_g = .221
+    protein_g_per_g = .175
+
+
+class Suet(Fat):
     fat_g_per_g = .946
     protein_g_per_g = .018
 
 
-def meal(energy_kc, protein_g, meat_fat_prop, liver_g):
-    liver = Liver(liver_g)
-    meat = Meat.from_protein_g(protein_g-liver.protein_g, meat_fat_prop)
-    suet = Suet(
-        (energy_kc - (liver.energy_kc + meat.energy_kc))
-        / Suet.energy_kc_per_g)
-    return liver, meat, suet
+def meal(foods, energy_kc, protein_g, organ_g):
+    fat, meat, organ = foods
+    organ.weight_g = organ_g
+    meat.weight_g = (protein_g - organ.protein_g) / meat.protein_g_per_g
+    fat.weight_g = (energy_kc - (organ.energy_kc + meat.energy_kc)) / fat.energy_kc_per_g
+    return foods
 
 
 def fat_prop_meal(energy_kc, fat_ratio, meat_fat_prop, liver_g):
@@ -75,31 +93,16 @@ def fat_prop(meal):
     protein_g= sum(i.protein_g for i in meal)
     return fat_g / protein_g
 
-def two_meats_meal(energy_kc):
-    meat1 = Meat.from_protein_g(25, .5)
-    meat2 = Meat.from_protein_g(65-25, .2)
-    suet = Suet((energy_kc - (meat1.energy_kc + meat2.energy_kc)) / Suet.energy_kc_per_g)
-    meal = (meat1, meat2, suet)
-    for i in meal: print(i)
-    print(fat_prop(meal))
-    exit();
-
 
 if __name__ == '__main__':
-    #two_meats_meal(1900)
-    if len(sys.argv) > 1:
-        fat_prop = int(sys.argv[1]) / 100
-        meat = Meat(1, fat_prop)
-        print(meat.fat_g/meat.protein_g)
-        exit()
-    liver_g_per_w = 454
-    liver_g_per_d = liver_g_per_w / 7
-    m =  meal(energy_kc=1900, protein_g=64, meat_fat_prop=.2, liver_g=liver_g_per_d)
+    fat = Suet()
+    meat = GB45()
+    organ = Liver()
+    foods = fat, meat, organ
+
+    organ_g_per_w = 454
+    organ_g_per_d = organ_g_per_w / 7
+    m =  meal(foods, energy_kc=1900, protein_g=64, organ_g=organ_g_per_d)
     for i in m:
         print(i)
     print('fat/protein ', fat_prop(m))
-
-    # m =  fat_prop_meal(energy_kc=1900, fat_ratio=2, meat_fat_prop=.2, liver_g=liver_g_per_d)
-    # for i in m:
-    #     print(i)
-    # print('fat/protein ', fat_prop(m))
