e97a53ae7ed38ca38f8c77594fb88a4cfd07e8bd
John de la Garza <jdelagarza@cylance.com>
2021-07-03 09:41:37 -0700

    change whitespace

--- calc.py
+++ calc.py
@@ -108,6 +108,7 @@ if __name__ == "__main__":
     offal = Liver(50)
     # 25g of brain or marrow
     offal2 = Marrow(25)
+
     meat = create_meat_class(fat_percent=25).from_protein_g(
         protein_g - offal.protein_g - offal2.protein_g
     )
@@ -116,6 +117,7 @@ if __name__ == "__main__":
     )
     foods = fat, meat, offal, offal2
     foods = [f.__class__(f.weight_g / 2) for f in foods]
+
     for food in foods:
         if food.weight_g:
             print(food)
