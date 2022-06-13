c267f63480199e1269793807473227e78db15299
John de la Garza <jdelagarza@cylance.com>
2022-06-13 07:35:35 -0700

    remove non used code

--- calc.py
+++ calc.py
@@ -10,10 +10,6 @@ class Food:
         self.protein_g = self.protein_g_per_g * weight_g
         self.weight_g = weight_g
 
-    def __new__(cls, *args, **kwargs):
-        assert cls is not Food, "Cannot instantiate Food"
-        return super().__new__(cls)
-
     @classmethod
     def from_protein_g(cls, protein_g):
         return cls(protein_g / cls.protein_g_per_g)
@@ -27,58 +23,11 @@ class Fat(Food):
     protein_g_per_g = 0.018
 
 
-class Offal(Food):
-    def __init__(self, weight_g):
-        if type(self) is __class__:
-            raise TypeError("abstract class")
-        super().__init__(weight_g)
-
-
-class Brain(Offal):
-    fat_g_per_g = 0.1
-    protein_g_per_g = 0.1
-
-
-class Marrow(Offal):
-    fat_g_per_g = 0.84
-    protein_g_per_g = 0.067
-
-
-class Kidney(Offal):
-    fat_g_per_g = 0.031
-    protein_g_per_g = 0.174
-
-
-class Spleen(Offal):
-    fat_g_per_g = 0.03
-    protein_g_per_g = 0.18
-
-
-class SweetBread(Offal):
-    fat_g_per_g = 0.204
-    protein_g_per_g = 0.122
-
-
-class Liver(Offal):
-    fat_g_per_g = 0.036
-    protein_g_per_g = 0.204
-
-
-class Tendon(Offal):
-    fat_g_per_g = 0.05
-    protein_g_per_g = 0.367
-
-
-class Egg(Offal):
-    fat_g_per_g = 0.099
-    protein_g_per_g = 0.126
-
-
 def create_meat_class(fat_percent):
     fat_decimal = fat_percent / 100
     lean_decimal = 1 - fat_decimal
     return type(
-        f"Meat_{fat_percent}",
+        f"Meat{fat_percent}",
         (Food,),
         dict(
             fat_g_per_g=fat_decimal * 0.98,
@@ -94,30 +43,21 @@ def fat_prop(foods):
 
 
 def energy_total(foods):
-    return sum(
-        [
-            sum(food.fat_g * 9 for food in foods),
-            sum(food.protein_g * 4 for food in foods),
-        ]
+    return sum(food.fat_g * 9 for food in foods) + sum(
+        food.protein_g * 4 for food in foods
     )
 
 
 if __name__ == "__main__":
-    fat_g = 200
-    protein_g = 75
-    offal = Liver(50)
-    # 25g of brain or marrow
+    fat_g = 215
+    protein_g = 90
 
-    meat = create_meat_class(fat_percent=25).from_protein_g(
-        protein_g - offal.protein_g
-    )
-    fat = Fat((fat_g - offal.fat_g - meat.fat_g) / Fat.fat_g_per_g)
-    foods = fat, meat, offal
+    meat = create_meat_class(fat_percent=12).from_protein_g(protein_g)
+    fat = Fat((fat_g - meat.fat_g) / Fat.fat_g_per_g)
+    foods = fat, meat
     foods = [f.__class__(f.weight_g / 2) for f in foods]
 
     for food in foods:
-        if food.weight_g:
-            print(food)
-
+        print(food)
     print("fat/protein ", fat_prop(foods))
     print("energy", energy_total(foods))
