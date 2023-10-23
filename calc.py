170ae7b83339a7001adef7286a933fe069180549
John de la Garza <john@jjdev.com>
2023-10-23 12:39:37 -0700

    calc: consolidate meats

--- calc.py
+++ calc.py
@@ -81,15 +81,12 @@ def fat_total(foods):
 
 
 if __name__ == "__main__":
-    ground_beef_fat = 25
-    prime_rib_fat = 16
-    top_sirloin_fat = 5
-
+    beef_fat_percent = 25
     protein_g = 82
     offal = Liver(0)
     fat_g = 288
 
-    meat = create_meat_class(fat_percent=ground_beef_fat).from_protein_g(
+    meat = create_meat_class(fat_percent=beef_fat_percent).from_protein_g(
         protein_g - offal.protein_g
     )
     fat = Fat((fat_g - offal.fat_g - meat.fat_g) / Fat.fat_g_per_g)
