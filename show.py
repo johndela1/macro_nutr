8ca4aaf8519ab5f222bb5846dfbe6e36ca623bbd
John de la Garza <john@jjdev.com>
2024-01-14 09:11:43 -0800

    show: add

---
+++ show.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from calc import *
+
+if __name__ == "__main__":
+    offal = Brain(25)
+    meat = create_meat_class(fat_percent=45)(280)
+    meat2 = create_meat_class(fat_percent=25)(33)
+    fat = Fat(0)
+    foods = fat, meat, meat2, offal
+
+    for food in foods:
+        print(food)
+    print()
+    print("total fat", fat_total(foods))
+    print("total protein", protein_total(foods))
+    print("fat/protein ", fat_prop(foods))
+    print("energy", energy_total(foods))
