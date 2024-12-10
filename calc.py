6a936ca9b8a703c340ab3a12bc5acf903b7e5086
John de la Garza <john@jjdev.com>
2024-12-10 04:16:49 -0800

    change fat decimal for Chuck

--- calc.py
+++ calc.py
@@ -61,7 +61,7 @@ def create_meat_class(fat_decimal, name="Ground"):
 
 Ribeye = create_meat_class(fat_decimal=0.20, name="Ribeye")
 Lamb = create_meat_class(fat_decimal=0.12, name="Lamb")
-Chuck = create_meat_class(fat_decimal=0.11, name="Chuck")
+Chuck = create_meat_class(fat_decimal=0.13, name="Chuck")
 Meat25 = create_meat_class(fat_decimal=0.25)
 
 
